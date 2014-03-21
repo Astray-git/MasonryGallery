9719786e8293b57309cbf8f85b7f6e95188d3e62
Astray-git <lyc001wow@gmail.com>
2014-03-22 01:29:06 +0800

    Infinite scroll loading indicator etc.

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var photos = [];
+var pageNum = 2;
+var container = document.getElementsByClassName('container')[0];
+var prevScrollPos = document.body.scrollTop;
+var moreUrl='https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=2202999fc9c18d1e5168b73f8cc440de&user_id=14738778%40N03&extras=url_m&per_page=20&page='+ pageNum +'&format=json';
+
 // jsonp callback function
 function jsonFlickrApi(obj) {
   photos = photos.concat(obj.photos.photo);
@@ -16,16 +21,8 @@ var getPics = function(url, init) {
   }
 };
 
-var button = document.getElementsByTagName('button')[0];
-var pageNum = 2;
-button.addEventListener('click', function() {
-  var url='https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=2202999fc9c18d1e5168b73f8cc440de&user_id=14738778%40N03&extras=url_m&per_page=20&page='+ pageNum +'&format=json'
-  getPics(url, false);
-  pageNum++;
-});
-
 var picRearrange = function(col) {
-  var wrappers = document.getElementsByClassName('pic-wrapper'),
+  var wrapper = document.getElementsByClassName('pic-wrapper'),
     absoluteLeft = ['0', '250px', '500px', '750px', '1000px', '1250px', '1500px'],
     totalHeight = [0,0,0,0,0,0,0];
 
@@ -33,11 +30,11 @@ var picRearrange = function(col) {
     return Math.max.apply( Math, array );
   };
 
-  Array.prototype.forEach.call(wrappers, function(element, index, array) {
+  Array.prototype.forEach.call(wrapper, function(element, index, array) {
     var oWidth = photos[index].width_m,
       oHeight = photos[index].height_m,
-      cWidth = 236,
-      cHeight = Math.round(oHeight * cWidth / oWidth),
+      eWidth = 236,
+      eHeight = Math.round(oHeight * eWidth / oWidth),
       title = document.getElementsByClassName('title'),
       tHeight = title[index].clientHeight;
 
@@ -51,28 +48,27 @@ var picRearrange = function(col) {
       if(index > rIndex) {
         element.style.top = totalHeight[rIndex - 1].toString() + 'px';
       }
-      totalHeight[rIndex - 1] += cHeight + tHeight + 20;
+      totalHeight[rIndex - 1] += eHeight + tHeight + 20;
     } else {
       element.style.left = absoluteLeft[col - 1];
       if(index > rIndex) {
         element.style.top = totalHeight[col - 1].toString() + 'px';
       }
-      totalHeight[col - 1] += cHeight + tHeight + 20;
+      totalHeight[col - 1] += eHeight + tHeight + 20;
     }
 
     if(index == array.length - 1) {
-      var container = document.getElementsByClassName('container')[0];
       container.style.height = (arrayMax(totalHeight) + 30).toString() + 'px';
     }
   });
+
+  infiniteScroll(loadMore);
 };
 
 var insPics = function() {
   var templateScript = document.getElementById('pics-template').innerHTML;
   var template = Handlebars.compile(templateScript);
-  var container = document.getElementsByClassName('container')[0];
   container.innerHTML = template(photos);
-  var imgs = document.getElementsByTagName('img');
 
   // MediaQuery
   var query1 = "screen and (min-width:1751px)",
@@ -96,7 +92,6 @@ var insPics = function() {
         match : function() { picRearrange(3); }
       };
 
-
   var addMQListener = function() {
     enquire
       .register(query5, handler5)
@@ -106,7 +101,55 @@ var insPics = function() {
       .register(query1, handler1);
   };
 
+  var imgs = document.getElementsByTagName('img');
   imgs.onload = addMQListener();
+};
+
+var infiniteScroll = function(handler) {
+  var scroller = {
+    handler: handler,
+    updateInitiated: false
+  };
+  window.onscroll = function(event) {
+      scrollHandler(scroller, event);
+  };
+
+  // Touch devices
+  document.ontouchmove = function(event) {
+    scrollHandler(scroller, event);
+  };
+};
+
+function scrollHandler(scroller, event) {
+  if(scroller.loadInitiated) {
+    return;
+  }
+
+  /*var scrollPos = document.body.scrollTop;
+    chrome ok
+    Moz: document.body.scrollTop = 0
+  */
+  var scrollPos = window.pageYOffset;
+  if(scrollPos == prevScrollPos) {
+    return;
+  }
+
+  var pageHeight = document.documentElement.scrollHeight;
+  var clientHeight = window.innerHeight;
+
+  if(pageHeight - clientHeight - scrollPos < 50) {
+    scroller.loadInitiated = true;
+  
+    scroller.handler(function() {
+      scroller.loadInitiated = false;
+    });
+  }
+  prevScrollPos = scrollPos;
+}
+
+var loadMore = function() {
+  getPics(moreUrl, false);
+  pageNum++;
 };
 
 var initUrl = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=2202999fc9c18d1e5168b73f8cc440de&user_id=14738778%40N03&extras=url_m&per_page=20&format=json';
