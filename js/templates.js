9754f578706a703356e5ec65785e15420df47098
Astray-git <lyc001wow@gmail.com>
2014-03-25 20:28:08 +0800

    lightbox

--- js/templates.js
+++ js/templates.js
@@ -1,16 +1,1 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['pics'] = template({"1":function(depth0,helpers,partials,data) {
-  var helper, functionType="function", escapeExpression=this.escapeExpression;
-  return "\r\n  <div class=\"pic-wrapper\">\r\n    <div class=\"pic\">\r\n      <img src=\""
-    + escapeExpression(((helper = helpers.url_m || (depth0 && depth0.url_m)),(typeof helper === functionType ? helper.call(depth0, {"name":"url_m","hash":{},"data":data}) : helper)))
-    + "\" alt=\"photos\">\r\n    </div>\r\n    <div class=\"title\">\r\n      <p>"
-    + escapeExpression(((helper = helpers.title || (depth0 && depth0.title)),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper)))
-    + "</p>\r\n    </div>\r\n  </div>\r\n";
-},"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
-  var stack1;
-  stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
-  if(stack1 || stack1 === 0) { return stack1; }
-  else { return ''; }
-  },"useData":true});
-})();
+!function(){var a=Handlebars.template,e=Handlebars.templates=Handlebars.templates||{};e.pics=a({1:function(a,e,n,r){var p,t="function",i=this.escapeExpression;return'\r\n  <div class="pic-wrapper">\r\n    <a class="pic" id="pic-'+i((p=null==r||r===!1?r:r.index,typeof p===t?p.apply(a):p))+'" href="'+i((p=a&&a.url_z,typeof p===t?p.apply(a):p))+'">\r\n      <img src="'+i((p=a&&a.url_z,typeof p===t?p.apply(a):p))+'" alt="photos">\r\n    </a>\r\n    <div class="title">\r\n      <p>'+i((p=a&&a.title,typeof p===t?p.apply(a):p))+"</p>\r\n    </div>\r\n  </div>\r\n"},compiler:[5,">= 2.0.0"],main:function(a,e,n,r){var p;return p=e.each.call(a,a,{name:"each",hash:{},fn:this.program(1,r),inverse:this.noop,data:r}),p||0===p?p:""},useData:!0})}();
